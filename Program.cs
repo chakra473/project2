514096bdcec3f8b7b4ff10f763b99608932cff3e
chakra473 <91408040+chakra473@users.noreply.github.com>
2022-03-11 16:57:42 +0530

    [chakra:]added sanjay  project in master branch

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace project2
             int num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"these numbers are divisible by {num}");
             div(num);
+            MyMethod(num);
         }
         public static void div(int num)
         {
@@ -22,6 +23,21 @@ namespace project2
                 }
 
             }
+        }
+         public static void MyMethod(int num)
+        {
+            int i=2;
+           
+            for (i = 2; i > 1; i++)
+            {
+
+                while (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                    num = num / i;
+                }
+                
+            }   
         }
     }
 }
