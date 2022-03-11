4aef7cbafe37de38de983f4cc77130bcd63d7932
Praful <prafulrakhade02@gmail.com>
2022-03-11 18:40:45 +0530

    [praful] Added: Factorial program file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        
+        public static void fact(int num)
+        {
+            long result = 1;
+            for (int i=1;i<=num;i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number to check factorial number");
+            int  num =int.Parse(Console.ReadLine());
+            fact(num);
+                
+        }
+    }
+}
