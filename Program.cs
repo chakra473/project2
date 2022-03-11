f6e83bf8b43adccdcbee9d53fd4b2209abdb7acf
chakravarthy <m.chakra473@gmail.com>
2022-03-11 15:37:24 +0530

    [chakra:] added divisible project to the branch

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace project2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"these numbers are divisible by {num}");
+            div(num);
+        }
+        public static void div(int num)
+        {
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
 }
