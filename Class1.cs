c40e9d8456032c508cdd5bdf821ec0fa6d091223
sanjay <ssah6694@gmail.com>
2022-03-11 15:54:15 +0530

    [sanjay]add: added prime factor

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class Class1
+    {
+        public void MyMethod(int num)
+        {
+            int i=2;
+           
+            for (i = 2; i > 1; i++)
+            {
+
+                while (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                    num = num / i;
+                }
+                
+            }   
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeFactors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            Console.WriteLine("Please ente your number");
+            int num = int.Parse(Console.ReadLine());
+            obj.MyMethod(num);
+        }
+    }
+}
